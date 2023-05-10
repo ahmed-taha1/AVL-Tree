73845a29d3c2a6ec38ecd85271b908513f18eb32
ahmed taha <bjabotaha@yahoo.com>
2023-05-10 23:54:06 +0300

    edit in data base

--- main.cpp
+++ main.cpp
@@ -4,9 +4,16 @@ using namespace std;
 
 int main(){
     AVLTree<int> test;
-    test.insert(12);
-    test.insert(5);
     test.insert(1);
+    test.insert(2);
+    test.insert(3);
+    test.insert(4);
+    test.insert(5);
+    test.insert(6);
+    test.insert(7);
+    test.insert(8);
+    test.insert(9);
+    test.insert(10);
     test.deleteNode(1);
     test.print();
 }
